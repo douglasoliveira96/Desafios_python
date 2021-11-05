889828a94fe450ac118065f4bbec5ea20edb3e0e
Douglas de Oliveira <douglasdeoliveiradd@gmail.com>
2021-11-05 17:32:54 -0300

    Desafio 71
    Fim do mundo 2.
    05/11/2021

--- desafio071.py
+++ desafio071.py
@@ -1,7 +1,7 @@
 '''Crie um programa que simule o funcionamento de um caixa eletrônico.
 No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro)
 e o programa vai informar quantas cédulas de cada valor serão entregues.
-OBS: considere que o caixa possui cédulas de R$100, 50, R$20, R$10 e R$1.'''
+OBS: considere que o caixa possui cédulas de R$100 R$50, R$20, R$10 R$5 e R$2.'''
 
 print('=' * 30)
 print('{:^30}'.format('\033[1;32mBANCO CEV\033[m'))
@@ -9,14 +9,15 @@ print('=' * 30)
 valor = int(input('Qual o valor do saque? R$'))
 total = valor
 céd = 100
-totcéd = 0 
+totcéd = 0
+
 while True:
-    if valor >= céd:
-        valor -= céd
+    if total >= céd:
+        total -= céd
         totcéd += 1
     else:
         if totcéd > 0:
-            print(f'Total de {totcéd} cédulas de {céd}R$.')
+            print(f'Total de {totcéd} cédulas de R${céd}.')
         if céd == 100:
             céd = 50
         elif céd == 50:
@@ -24,8 +25,12 @@ while True:
         elif céd == 20:
             céd = 10
         elif céd == 10:
+            céd = 5
+        elif céd == 5:
+            céd = 2
+        elif céd == 2:
             céd = 1
         totcéd = 0
         if total == 0:
             break
-print('Volte sempre')
+
