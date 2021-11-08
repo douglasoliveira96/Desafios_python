bae58b4e727b51a297afa02112c698bc411bd3e3
Douglas de Oliveira <douglasdeoliveiradd@gmail.com>
2021-11-08 18:50:51 -0300

    Desafio 76
    08/11/2021

---
+++ desafio076.py
@@ -0,0 +1,18 @@
+'''Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência.
+No final, mostre uma listagem de preços, organizando os dados em forma tabular.'''
+
+listagem = ('Lápis', 1.75,
+            'Borracha', 2,
+            'Caderno', 15.90,
+            'Estojo', 25,
+            'Transferidor', 4.20,
+            'Compasso', 9.90,
+            'Mochila', 120.50,
+            'Canetas', 2.30,
+            'Livro', 34.90)
+
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}')
+    else:
+        print(f'R${listagem[pos]:>10}')
